91d5048421b9fe1bf809e054a5c51ea7f3a2f3c7
Devi Raju <deviraju999@gmail.com>
2022-09-26 16:34:58 -0700

    changed function order in index.js

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
 const head   = require('./head');
-const tail   = require('./tail');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
 const middle = require('./middle');
+const tail   = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+
+
 
 module.exports = {
   head,
   tail,
-  middle
+  middle,
+  assertEqual,
+  eqArrays,
+  assertArraysEqual,
+  eqObjects,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  letterPositions,
+  findKey,
+  findKeyByValue,
+  map,
+  takeUntil,
+  without
 };
