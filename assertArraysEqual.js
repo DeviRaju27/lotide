341019380a515bffb469ca6717d4de2d51cdabe5
Devi Raju <deviraju999@gmail.com>
2022-09-25 20:53:43 -0700

    updated with for loop

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,20 @@
 const eqArrays = function (array1, array2) {
-  let arr1 = JSON.stringify(array1);
-  let arr2 = JSON.stringify(array2);
-  if (arr1 === arr2) {
-    return (true);
-  } else {
-    return (false);
-  }
-}
 
-const assertArrayEqual = function(arr1,arr2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1,arr2) {
 
   if (eqArrays(arr1,arr2)) {
     console.log(`✅✅✅assertion passed: ${arr1} === ${arr2}` );
@@ -19,4 +25,4 @@ const assertArrayEqual = function(arr1,arr2) {
 
 };
 
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
