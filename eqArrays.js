b59fa0eb339a0a5282dd412d416297084508f5bb
Devi Raju <deviraju999@gmail.com>
2022-09-25 20:52:15 -0700

    updated with for loop

--- eqArrays.js
+++ eqArrays.js
@@ -5,18 +5,24 @@ const assertEqual = function (actual, expected) {
     console.log(`❌❌❌assertion failed: ${actual} !=== ${expected}`);
   }
 };
-const eqArrays = function (array1, array2) {
-  let arr1 = JSON.stringify(array1);
-  let arr2 = JSON.stringify(array2);
-  if (arr1 === arr2) {
-    return (true);
-  } else {
-    return (false);
-  }
-}
+const eqArrays = function(array1, array2) {
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1]))// => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
